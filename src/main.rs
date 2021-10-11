748a691cbc9f90f9c241464d0aebe36fd87bee03
kimyk0120 <kimyk0120@hotmail.com>
2021-10-12 08:59:21 +0900

    * rust test

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,28 @@
 
+use std::io;
+
+
 fn main() {
-    println!("Hello, world!");
-    test();
+    println!("Guess the number!");
+
+    println!("Please input your guess.");
+
+    let mut guess = String::new();
+
+    io::stdin().read_line(&mut guess)
+        .expect("Failed to read line");
+
+    println!("You guessed: {}", guess);
 
 }
 
 fn test(){
     println!("test2");
+    let mut count = 1;
+    while count < 10 {
+
+        count += 1;
+    }
 }
 
 
