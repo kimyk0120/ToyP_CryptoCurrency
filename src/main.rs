1c82b217452e8aba9a3fd0319ac74652a484409a
kimyk0120 <kimyk0120@hotmail.com>
2021-11-11 14:44:22 +0900

    no message

--- src/main.rs
+++ src/main.rs
@@ -3,26 +3,14 @@ use std::io;
 
 
 fn main() {
-    println!("Guess the number!");
-
-    println!("Please input your guess.");
-
-    let mut guess = String::new();
-
-    io::stdin().read_line(&mut guess)
-        .expect("Failed to read line");
-
-    println!("You guessed: {}", guess);
-
+    let unconfirmed_transactions  = [1, 2];
+    println!("test");
 }
 
-fn test(){
-    println!("test2");
-    let mut count = 1;
-    while count < 10 {
-
-        count += 1;
-    }
+fn test() -> i32 {
+    let a = 10;
+    let b = 10;
+    return a + b;
 }
 
 
